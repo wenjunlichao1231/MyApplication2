ae7287975399500de58bc61ba5f2c6299c9945ad
wenjunlichao1231 <lichao@lichaodeMacBook-Pro.local>
2017-10-30 15:32:10 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyLibraryTest.java
@@ -0,0 +1,11 @@
+package com.example.mylibrary;
+
+/**
+ * Created by lichao on 2017/10/30.
+ */
+
+public class MyLibraryTest {
+    public String test(){
+        return "MyLibraryTest";
+    }
+}
